fc398630481b296615932a3040dc746ec0b72d5b
maimatsushima <38389622+maimatsushima@users.noreply.github.com>
2019-02-26 02:27:45 +0900

    slackに詳細載せます

--- looky/js/template.js
+++ looky/js/template.js
@@ -9,11 +9,11 @@ html += '</header>';
 document.write(html);
 }
 
-// index.html/more button
-function more(){
+//header2
+function headerNavy(){
 var html = "";
-html += '<div class="button-more">';
-html += '<a href="#">もっと見る</a>';
+html += '<div class="header-logo-navy">';
+html += '<h1><img src="../img/logo-navy.png" alt="logo-navy"></h1>';
 html += '</div>';
 document.write(html);
 }
@@ -22,27 +22,31 @@ document.write(html);
 function footer(){
 var html = "";
 html += '<footer>';
+html += '<div class="footer1">';
 html += '<div class="footer-menu">';
 html += '<ul>';
 html += '<li><a href="index.html">web業界仕事図鑑</a></li>';
 html += '<li><a href="magazine.html">マガジン</a></li>';
 html += '<li><a href="lesson.html">レッスン</a></li>';
-html += '<li><a href="diagnosis.html">診断</a></li>';
+html += '<li><a href="diagnosis.html">性格診断</a></li>';
 html += '<div class="footer-logo-orange">';
 html += '<img src="../img/logo-orange.png" alt="logo-orange">';
 html += '</div>';
 html += '</ul>';
 html += '</div>';
-html += '<p><small>&copy;copyright looky All Right Reserved</small></p>';
+html += '<p><small>&copy;copyright looky all right reserved</small></p>';
+html += '</div>';
 html += '</footer>';
 document.write(html);
 }
 
-// footer-short
-function footer2(){
-var html = "";
-html += '<footer>';
-html += '<p><small>&copy;copyright looky All Right Reserved</small></p>';
-html += '</footer>';
-document.write(html);
-}
+// // footer-short
+// function footer2(){
+// var html = "";
+// html += '<footer>';
+// html += '<div class="footer2">';
+// html += '<p><small>&copy;copyright looky all right reserved</small></p>';
+// html += '</div>';
+// html += '</footer>';
+// document.write(html);
+// }
