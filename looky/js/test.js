1f4c25dbdbc8435964003311313a97f261dd282c
maimatsushima <38389622+maimatsushima@users.noreply.github.com>
2019-02-27 16:36:34 +0900

    細かい修正、menuテスト中です

--- looky/js/test.js
+++ looky/js/test.js
@@ -2,12 +2,12 @@ $(function () {
 
 	$('.zdo_drawer_button').click(function () {
 		$(this).toggleClass('active');
-		$('.zdo_drawer_bg').fadeToggle();
-		$('nav').toggleClass('open');
+		// $('.zdo_drawer_bg').fadeToggle();
+		$('.zdo_drawer_nav_wrapper').toggleClass('open');
 	})
-	$('.zdo_drawer_bg').click(function () {
-		$(this).fadeOut();
-		$('.zdo_drawer_button').removeClass('active');
-		$('nav').removeClass('open');
-	});
+	// $('.zdo_drawer_bg').click(function () {
+	// 	$(this).fadeOut();
+	// 	$('.zdo_drawer_button').removeClass('active');
+	// 	$('nav').removeClass('open');
+	// });
 })
